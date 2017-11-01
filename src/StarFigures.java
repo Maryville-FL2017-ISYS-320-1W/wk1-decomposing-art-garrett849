7c6bba14b1cfe8898b9dfec8a80cd574d9229ffe
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-01 16:46:14 -0500

    stars figure 1

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -11,17 +11,41 @@ public class StarFigures {
 
 	public static void main(String[] args) {
 		draw2lines();
-	
-
+		drawX();
+		System.out.println("");
+		System.out.println("");
+		draw2lines();
+		drawX();
+		draw2lines();
+		System.out.println("");
+		System.out.println("");
+		draw_vertical_line();
+		draw2lines();
+		drawX();
 	}
 	
-	//Make two lines of stars
+	//Make two lines of stars.
 	public static void draw2lines() {
 		for(int i = 1; i <= 2; i++) {
 			for(int j = 1; j <= 5; j++) {
 			System.out.print("*");
-		}
+			}
 			System.out.println();
+		}
 	}
-}
+	//Draw a X.
+	public static void drawX() {
+		System.out.println(" * *");
+		System.out.println("  *");
+		System.out.println(" * *");
+	}
+	//Draw vertical line.
+	public static void draw_vertical_line() {
+		for(int i = 1; i <= 3; i++) {
+			for(int j = 1; j <= 1; j++) {
+			System.out.print("  *");
+			}
+			System.out.println();
+			}
+	}	
 }
